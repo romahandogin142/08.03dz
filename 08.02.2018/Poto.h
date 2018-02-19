7fefd0bdd5ecd079f92e4179bb177881d1a55ac7
Samsung <Samsung@DESKTOP-T2L8ES4>
2018-02-19 20:32:36 +0600

    Добавьте файлы проекта.

---
+++ 08.02.2018/Poto.h
@@ -0,0 +1,8 @@
+void head(char mas[]);
+double f(double x, double y);
+double f(double x1, double y1, double x2, double y2);
+int f(int a);
+int f(int a, int b);
+int f(int a, int b, int c);
+void swap(int* a, int* b);
+void swap(int& a, int& b);
